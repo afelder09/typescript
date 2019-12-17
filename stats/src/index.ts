2256d4e34c2f8484c732f583d2d10cbef7d4d490
Anson Felder <ansonfelder@Ansons-MacBook-Pro.local>
2019-12-17 17:18:55 -0500

    Finishing stats project

---
+++ stats/src/index.ts
@@ -0,0 +1,10 @@
+
+import { MatchReader } from './MatchReader'
+import { Summary } from './Summary'
+
+const matchReader = MatchReader.fromCsv('football.csv')
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
+
+summary.buildAndPrintReport(matchReader.matches)
