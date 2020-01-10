ed7e5b2ca386129fd9ecca682aa5adfc6f4a5da7
Anson Felder <ansonfelder@Ansons-MacBook-Pro.local>
2020-01-10 11:36:10 -0500

    Finished the web application project

--- web/src/index.ts
+++ web/src/index.ts
@@ -6,8 +6,6 @@ const users = new Collection('http://localhost:3000/users', (json: UserProps) =>
     return User.buildUser(json)
 })
 
-console.log(users)
-
 users.on('change', () => {
     const root = document.getElementById('root')
     if (root) {
